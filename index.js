7ebac75d1f9f47ee75d25766d1e0d6bddbd1f5fe
YuliyaKh <anelli6539@gmail.com>
2023-07-01 16:54:37 +0300

    удаление ссылок

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// https://lk.staropetrovskoe.ru/api/catalog/get_groups.json?token=cd95af1c-efb9-4928-a985-9f17bb10b776
-// мск https://lk.staropetrovskoe.ru/api/catalog/get_elements.json?token=cd95af1c-efb9-4928-a985-9f17bb10b776&parent=%D0%A10003114&store=028
-// крд https://lk.staropetrovskoe.ru/api/catalog/get_elements.json?t=54b55f6c3d693a07623efce7ea39cb7b&parent=%D0%A10003114&store=037
-
 const fs = require("fs");
 
-fetch('http://lk.staropetrovskoe.ru/api/catalog/get_groups.json?token=cd95af1c-efb9-4928-a985-9f17bb10b776')
+fetch('http://')
   .then(res => res.json())
   .then(obj => {
       let ids = [];
@@ -23,7 +19,7 @@ fetch('http://lk.staropetrovskoe.ru/api/catalog/get_groups.json?token=cd95af1c-e
 
       getId(obj);
 
-      let requests = ids.map(id => fetch(`http://lk.staropetrovskoe.ru/api/catalog/get_elements.json?token=cd95af1c-efb9-4928-a985-9f17bb10b776&parent=${id}&store=028`));
+      let requests = ids.map(id => fetch(`http://${id}&`));
 
       Promise.all(requests)
         .then(res => {
